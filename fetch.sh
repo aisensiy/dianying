2c94b28d8b35acac9f305887df95460e98ef2de7
aisensiy <aisensiy@gmail.com>
2014-01-21 14:37:45 +0800

    add export for shell

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
+
 echo "$(date)" >> /home/bae/log/cron.log
+set -x
 cd /home/bae/app
-PYTHONPATH=$PYTHONPATH:/home/bae/app
+export PYTHONPATH=$PYTHONPATH:/home/bae/app
 python /home/bae/app/get_douban_movies.py 2>&1 >> /home/bae/log/cron.log
