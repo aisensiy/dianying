f32d3e3c58322bc18ba85982e8075f720a69464c
aisensiy <aisensiy@gmail.com>
2014-01-21 15:47:49 +0800

    add log path

--- fetch.sh
+++ fetch.sh
@@ -3,8 +3,8 @@
 # workpath=/Users/xushanchuan/projects/dianying
 workpath=/home/bae/app
 
-echo "$(date)" >> $workpath/cron.log
+echo "$(date)" >> $workpath/log/cron.log
 set -x
 cd $workpath
 export PYTHONPATH=$PYTHONPATH:$workpath
-python $workpath/get_douban_movies.py 2>&1 >> $workpath/cron.log
+python $workpath/get_douban_movies.py 2>&1 >> $workpath/log/cron.log
