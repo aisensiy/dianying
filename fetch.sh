ad36acdecb6b8a6824c52d822c29c1970c13afc4
aisensiy <aisensiy@gmail.com>
2014-01-21 14:21:46 +0800

    add cd and pythonpath

--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,4 @@
 echo "$(date)" >> /home/bae/log/cron.log
-python /home/bae/app/test.py 2>&1 >> /home/bae/log/cron.log
+cd /home/bae/app
+PYTHONPATH=$PYTHONPATH:/home/bae/app
+python /home/bae/app/get_douban_movies.py 2>&1 >> /home/bae/log/cron.log
